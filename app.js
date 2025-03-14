d80c9dfb763c9c45d672c0290fe0b4ff27347493
Raul Riquelme <riquelmraul@gmail.com>
2025-03-13 23:33:07 -0300

    Se actualiza app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let inputElement = document.getElementById('amigo');
+    let nuevoAmigo = inputElement.value.trim()
+
+    if (nuevoAmigo !== "") {
+        amigos.push(nuevoAmigo)
+        inputElement.value = ""
+        listarAmigos()
+
+    } else {
+        console.log("ingrese un nombre valido")
+    }
+
+
+}
+function listarAmigos() {
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ''
+
+    amigos.forEach(amigo => {
+        let li = document.createElement('li')
+        li.textContent = amigo
+        lista.appendChild(li)
+    })
+}
